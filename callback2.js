3f863d3aa18b4f4d8b509046111563bd84717934
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-04 18:58:26 +0900

    두번째 콜백 주석 추가

--- callback2.js
+++ callback2.js
@@ -7,3 +7,5 @@ function sayHello(name, callback) {
 sayHello("인파", function(name) { //함수의 이름이 없는 익명 함수
   console.log(name)
 })
+// name을 인파로 해두었고, 1번과 다르게 function 이후 함수명을 지정을 안했다.
+// 안한상황에서 console.log(name)을 지정했더니 첫번째 선언에서의 함수를 불러와 답을 선언했다.
