d073b6b3d6ee00d6783021378eb93f21d7f74919
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-04 18:47:09 +0900

    첫번째 콜백 주석 추가

--- callback1.js
+++ callback1.js
@@ -3,7 +3,8 @@ function sayHello(name, callback) {
 
   callback(words);
 }
-
+// 콜백함수 name이란 매개변수를 사용하여 값이 나올 수 있도록 작업
 sayHello("인파", function printing(name) {
   console.log(name);
 })
+// 위 함수대로 sayHello를 사용하여 name에 인파란 단어를 호출하게 만들었다.
