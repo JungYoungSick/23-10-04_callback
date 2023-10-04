e6caa3824a0617b7383ff6d6b24812a24c8a426e
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-04 19:17:09 +0900

    세번째 콜백 add 주석 추가

--- callback3.js
+++ callback3.js
@@ -20,6 +20,7 @@ function sum(x, y, callback) {
 sum(1,2, function add(result) {
   //함수명 add 추가.
   console.log(result);
+  //함수명을 지정하지 않아도 값을 뱉어낸다.
 });
 //함수 안 작성한 콜백의 x=1, y=2이기에 결과값은 3이 나온다.
 console.log(add);
